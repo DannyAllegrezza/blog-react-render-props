76384565a4cb089bb612647fcb71e5e6548ed73b
Danny Allegrezza <danny@dannyallegrezza.com>
2020-02-18 23:23:10 -0500

    stub out layout component

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ const navItems = [
   }
 ];
 
+const Layout = props => {
+  return (
+    <div className="App">
+      <div className="Sidebar">
+        <ul />
+      </div>
+    </div>
+  );
+};
 const NavItem = props => {};
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Layout />;
 }
